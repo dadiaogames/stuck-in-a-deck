a32899f5e2e1533f3b0c3c7b7f36d134991b1ee5
jakehsiao <1499975101@qq.com>
2020-10-05 05:11:56 +0800

    Updates

---
+++ precache-manifest.7e850b9d848515a24abb230713851fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a663adab309539b1e0040808698276",
+    "url": "/stuck-in-a-deck/index.html"
+  },
+  {
+    "revision": "1016990836a4e7d3dca8",
+    "url": "/stuck-in-a-deck/static/css/main.3542828a.chunk.css"
+  },
+  {
+    "revision": "2bd191b020588ce951ce",
+    "url": "/stuck-in-a-deck/static/js/2.919a1102.chunk.js"
+  },
+  {
+    "revision": "581070db7478fba8a52f7e9e4c335e11",
+    "url": "/stuck-in-a-deck/static/js/2.919a1102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1016990836a4e7d3dca8",
+    "url": "/stuck-in-a-deck/static/js/main.ab6374bd.chunk.js"
+  },
+  {
+    "revision": "9cea5b178879915c46a8",
+    "url": "/stuck-in-a-deck/static/js/runtime-main.21a06495.js"
+  }
+]);
